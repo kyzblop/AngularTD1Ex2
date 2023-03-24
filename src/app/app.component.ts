f5729254c5f93cd70fc4b4fabb5006b47627484c
kyzblop <jeremymoi.jd@gmail.com>
2023-03-24 16:20:30 +0100

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TD1Ex2';
+
+  imageUrl:string = "https://media.gqmagazine.fr/photos/61fbe6c83e6a79f4873ed6e8/4:3/w_1199,h_899,c_limit/One%20piece%201.jpg"
+
+  nomClasse:string= "redClass"
+  disbouton:boolean = true
+
+  clique(){
+    console.log("On a cliqué sur le bouton")
+  }
+  
+  detail:boolean = false
+ 
+  details(){
+    this.detail = true
+  }
+
+  detailNul(){
+    this.detail = false
+  }
+  
+  
 }
